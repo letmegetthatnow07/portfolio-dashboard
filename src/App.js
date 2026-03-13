4f2343b2f4b133a3dff730239f4771ee5abb1791
letmegetthatnow07 <letmegetthatnow07@gmail.com>
2026-03-13 12:37:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EnhancedPortfolioDashboard from './components/enhanced-portfolio-dashboard';
+
+function App() {
+  return <EnhancedPortfolioDashboard />;
+}
+
+export default App;
